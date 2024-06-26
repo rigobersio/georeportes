e2da85d3a7888dfbc3a9a30f64f79d41ab62741e
Rigoberto Antonio Maximiliano Martínez Quintana <rigomartinez@udec.cl>
2024-06-25 22:11:07 -0400

    (atlas)(server)

---
+++ server/src/routes/mPagoRouter.js
@@ -0,0 +1,11 @@
+const {Router} = require("express");
+const {postHandlerPreference} = require("../handlers/mPagoHandlers");
+
+const mPagoRouter = Router();
+
+
+mPagoRouter.post("/create-preference", postHandlerPreference);
+
+
+module.exports = mPagoRouter;
+
